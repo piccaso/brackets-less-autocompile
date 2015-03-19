05a53fa4481aedee14ffd85c74b3aea4dabfa176
Florian Fida <f@fida.biz>
2015-03-19 15:50:44 +0100

    Cross-platform make-package.sh

--- make-package.sh
+++ make-package.sh
@@ -1,1 +1,15 @@
-zip -9r ~/Desktop/pack.zip * -x \*.git example/\* make-package.sh
+if test -z "$(git status --porcelain --untracked-files=no)" ; then
+  cd node
+  git rm .gitignore 
+  rm -rf node_modules/
+  npm install
+  cd ..
+  git add --all
+  NODE=$(which node || which nodejs);
+  VERSION=$("$NODE" -e 'var pkg = require("./package.json"); console.log(pkg.version);')
+  git stash save "package-$VERSION"
+  git archive --worktree-attributes --format zip -9 -o "brackets-less-autocompile-$VERSION.zip" stash@{0}
+  git stash drop
+else
+  echo "Working directory is dirty..."
+fi
